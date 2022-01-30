3e62e5b04f8b01ca5f49476150772f593d8f3dcd
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-01-29 22:59:57 -0700

    Ultimos cambios de sintaxis

--- AnalizadorSintaxis/sintaxis.c
+++ AnalizadorSintaxis/sintaxis.c
@@ -6,23 +6,32 @@
 #include "cabecera.h"
 
 void SintaxisLeer(NODO *cabeza, int paso){
+
+}
+
+void SintaxisMostrar(NODO *cabeza, int paso){
     if(cabeza != NULL){
         switch(paso){
-            case 1:/* Espera un: "Leer" */
-                if (strcmp(cabeza->tok.lexema, "MOSTRAR") == 0)
-                    SintaxisLeer(cabeza->siguiente, 2);
+            case 'MOSTRAR':/* Espera un: "Leer" */
+                if (strcmp(cabeza->tok.lexema, "MOSTRAR") == 0){
+                    printf("Correcto 1");
+                    SintaxisLeer(cabeza->siguiente, 'PARENTUNO');
+                }
                 else{
                     printf("No lo encontro");
                 }
                 break;
-            case 2:/* Espera un: "Leer" */
-                printf("Ay la llevas guey");
+            case 'PARENTUNO':/* Espera un: "(" */
+                if (strcmp(cabeza->tok.lexema, "(") == 0){
+                    printf("Correcto 2");
+                    SintaxisLeer(cabeza->siguiente, 2);
+                    }
+                else{
+                    printf("No lo encontro");
+                }
                 break;
         }
     }
-}
-
-void SintaxisMostrar(NODO *cabeza, int paso){
 }
 
 void SintaxisMientras(NODO *cabeza, int paso){
