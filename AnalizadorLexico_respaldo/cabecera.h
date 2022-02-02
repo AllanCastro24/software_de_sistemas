31ee4747384c125f72356ff49b26ced0bac0feca
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-02-02 13:23:40 -0700

    respaldos

--- AnalizadorLexico_respaldo/cabecera.h
+++ AnalizadorLexico_respaldo/cabecera.h
@@ -1,23 +1,23 @@
 typedef enum{
-    PalRes,
-    ID,
-    NUM,
-    SIM,
-    OPAR,
-    OPCOMP,
-    OPASIGN,
-    OPLOG,
-    ESPECIAL,
-    CAD,
-    ERROR
+    PalRes, //0
+    ID, //1
+    NUM, //2
+    SIM, //3
+    OPAR, //4
+    OPCOMP, //5
+    OPASIGN, //6
+    OPLOG, //7
+    ESPECIAL, //8
+    CAD, //8
+    DESCONOCIDO //10
 }TipoToken;
 
 typedef enum{
-    INT,
-    STRING,
-    CHAR,
-    FLOAT,
-    OTRO
+    INT, //0
+    STRING, //1
+    CHAR, //2
+    FLOAT, //3
+    OTRO //4
 }TipoDato;
 
 typedef struct{
